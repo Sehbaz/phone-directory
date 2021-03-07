0fc892b1f46789257676bca6b394da356d03f290
Sehbaz Rafik <sehbazm@gmail.com>
2021-03-07 16:23:44 +0000

    App css added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from "./Header";
+import "./App.css";
 function App() {
   return (
     <div>
-      <Header />
-      <button>Add</button>
-      <div>
-        <span>Name</span>
-        <br />
-        <span>Phone</span>
+      <div className="component-body-container">
+        <Header />
+        <button className="custom-btn add-btn">Add</button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <br />
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
       </div>
     </div>
   );
